76d0483e0f27bfa64052271c96511c8cd7e139a0
Alexander Hering <dev.hering@gmail.com>
2023-05-08 18:26:01 +0200

    customized user script

--- stable-diffusion-webui/webui-user.sh
+++ stable-diffusion-webui/webui-user.sh
@@ -4,28 +4,29 @@
 #########################################################
 
 # Install directory without trailing slash
-#install_dir="/home/$(whoami)"
+export install_dir="/stable-diffusion-webui-container"
 
 # Name of the subdirectory
-#clone_dir="stable-diffusion-webui"
+export clone_dir="stable-diffusion-webui"
 
 # Commandline arguments for webui.py, for example: export COMMANDLINE_ARGS="--medvram --opt-split-attention"
-#export COMMANDLINE_ARGS=""
+#export COMMANDLINE_ARGS="--allow-code --xformers --device-id 0"
+export COMMANDLINE_ARGS="--allow-code --skip-install --xformers --device-id 0"
 
 # python3 executable
-#python_cmd="python3"
+export python_cmd="python3"
 
 # git executable
-#export GIT="git"
+export GIT="git"
 
 # python3 venv without trailing slash (defaults to ${install_dir}/${clone_dir}/venv)
-#venv_dir="venv"
+export venv_dir="/stable-diffusion-webui-container/venv"
 
 # script to launch to start the app
 #export LAUNCH_SCRIPT="launch.py"
 
 # install command for torch
-#export TORCH_COMMAND="pip install torch==1.12.1+cu113 --extra-index-url https://download.pytorch.org/whl/cu113"
+export TORCH_COMMAND="pip install torchvision==0.14.1 torchaudio==0.13.1 torch==1.13.1+cu117 --extra-index-url https://download.pytorch.org/whl/cu117"
 
 # Requirements file to use for stable-diffusion-webui
 #export REQS_FILE="requirements_versions.txt"
